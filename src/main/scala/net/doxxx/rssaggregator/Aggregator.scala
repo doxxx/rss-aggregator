fd58ebd9216988284a900b073fe057f20e537dbf
Gordon Tyler <gordon@doxxx.net>
2013-04-03 08:26:41 -0400

    Refactor Aggregator to use future instead of FeedFetcher actor.

--- src/main/scala/net/doxxx/rssaggregator/Aggregator.scala
+++ src/main/scala/net/doxxx/rssaggregator/Aggregator.scala
@@ -1,20 +1,22 @@
 package net.doxxx.rssaggregator
 
 import akka.actor.{ActorLogging, Props, Actor}
-import akka.event.Logging
 import akka.pattern._
 import akka.util.Timeout
 import model._
 import scala.concurrent.duration._
 import scala.collection.JavaConversions._
-import com.sun.syndication.feed.synd.{SyndContent, SyndEntry}
+import com.sun.syndication.feed.synd._
 import scala.util.{Failure, Success}
+import scala.concurrent._
+import com.sun.syndication.io.SyndFeedInput
+import java.io.InputStreamReader
+import java.net.URL
 
 class Aggregator extends Actor with ActorLogging {
   import Aggregator._
   import context.dispatcher
 
-  val feedFetcher = context.actorOf(Props[FeedFetcher], "feed-fetcher")
   val feedStorage = context.actorOf(Props[FeedStorage], "feed-storage")
   val articleStorage = context.actorOf(Props[ArticleStorage], "article-storage")
   val opmlImporter = context.actorOf(Props[OpmlImporter], "opml-importer")
@@ -38,16 +40,12 @@ class Aggregator extends Actor with ActorLogging {
     }
     case AddFeed(url) => {
       log.debug("Fetching feed {}", url)
-      (feedFetcher ? FeedFetcher.FetchFeed(url)).mapTo[FeedFetcher.Result] onComplete {
-        case Success(FeedFetcher.Result(syndFeed)) => {
+
+      fetchFeed(url).onComplete {
+        case Success(syndFeed) => {
           log.debug("Fetched feed {} containing {} articles", syndFeed.getTitle, syndFeed.getEntries.size())
-          // store feed in db
-          feedStorage ! FeedStorage.StoreFeed(Feed(url, syndFeed.getLink, syndFeed.getTitle, Option(syndFeed.getDescription)))
-          // store feed articles in db
-          syndFeed.getEntries.map(_.asInstanceOf[SyndEntry]).foreach { e =>
-            val contents = e.getContents.map(_.asInstanceOf[SyndContent].getValue).mkString("\n")
-            articleStorage ! ArticleStorage.StoreArticle(Article(url, e.getUri, e.getLink, e.getTitle, e.getAuthor, e.getPublishedDate, e.getUpdatedDate, contents))
-          }
+          storeFeed(url, syndFeed)
+          storeArticles(url, syndFeed)
         }
         case Failure(t) => {
           log.error(t, "Could not load feed {}", url)
@@ -58,7 +56,7 @@ class Aggregator extends Actor with ActorLogging {
       context.system.scheduler.scheduleOnce(1.hour, self, AddFeed(url))
     }
     case ImportOpml(opml) => {
-      (opmlImporter ? OpmlImporter.Import(opml)).mapTo[Seq[Feed]] onComplete {
+      (opmlImporter ? OpmlImporter.Import(opml)).mapTo[Seq[Feed]].onComplete {
         case Success(feeds) => {
           feeds.foreach { feed =>
             self ! AddFeed(feed.link)
@@ -68,6 +66,21 @@ class Aggregator extends Actor with ActorLogging {
           log.error(t, "Could not import OPML")
         }
       }
+    }
+  }
+
+  def fetchFeed(url: String): Future[SyndFeed] = future {
+    new SyndFeedInput().build(new InputStreamReader(new URL(url).openStream()))
+  }
+
+  def storeFeed(url: String, syndFeed: SyndFeed) {
+    feedStorage ! FeedStorage.StoreFeed(Feed(url, syndFeed.getLink, syndFeed.getTitle, Option(syndFeed.getDescription)))
+  }
+
+  def storeArticles(url: String, syndFeed: SyndFeed) {
+    syndFeed.getEntries.map(_.asInstanceOf[SyndEntry]).foreach { e =>
+      val contents = e.getContents.map(_.asInstanceOf[SyndContent].getValue).mkString("\n")
+      articleStorage ! ArticleStorage.StoreArticle(Article(url, e.getUri, e.getLink, e.getTitle, e.getAuthor, e.getPublishedDate, e.getUpdatedDate, contents))
     }
   }
 }
